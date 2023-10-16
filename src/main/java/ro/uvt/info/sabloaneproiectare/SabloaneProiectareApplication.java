75dacfc919f7cf36b4c222bee796e53d6133d16e
SilviuTM <silviutudor24@gmail.com>
2023-10-17 00:43:36 +0300

    Project initialization

---
+++ src/main/java/ro/uvt/info/sabloaneproiectare/SabloaneProiectareApplication.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.sabloaneproiectare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabloaneProiectareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SabloaneProiectareApplication.class, args);
+    }
+
+}
